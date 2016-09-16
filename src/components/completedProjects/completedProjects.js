f6988a0fc9168350b45c5d5b9f19fe46d7e126cc
AdamJackman <adam.jackman@mail.utoronto.ca>
2016-09-16 02:17:19 -0700

    pass at some improvements

--- src/components/completedProjects/completedProjects.js
+++ src/components/completedProjects/completedProjects.js
@@ -5,22 +5,27 @@ import './css/completedProjects.css'
 
 const pos1Title = "Personal Website";
 const pos1Text = "You are on it! Created with ES6 React.js. No bootstrap. No materialize. Just my css.";
+const pos1Git = "https://github.com/AdamJackman/PersonalSite-Redesign";
 import pos1Img from '../../logo.svg'
 const pos2Title = "Braille Printer";
 const pos2Text = "I made a Braille Printer with 2 other guys from UofT. It won first place for our capstone project";
-import pos2Img from '../../logo.svg'
+const pos2Git = "https://github.com/AdamJackman/BraillePrinter";
+import pos2Img from './content/braille.jpg'
 const pos3Title = "Seeds";
 const pos3Text = "I do a lot of personal projects. I've been making seeds to bootstrap new projects on a good stack.";
-import pos3Img from '../../logo.svg'
-const pos4Title = "TLA";
-const pos4Text = "Personal project scripting business specific logic. Made my development team much more efficient.";
-import pos4Img from '../../logo.svg'
-const pos5Title = "Password Manager";
-const pos5Text = "It creates strong reuseable site specific passwords from a simple input. Doesn't even need to save a hashed password";
-import pos5Img from '../../logo.svg'
-const pos6Title = "RIP Grooveshark";
-const pos6Text = "Chrome extension to track songs that would periodically get silently removed from your Grooveshark playlists.";
-import pos6Img from '../../logo.svg'
+const pos3Git = "https://github.com/AdamJackman/Finagle-Postgres-Slick-3.1-Seed";
+import pos3Img from './content/github.jpg';
+const pos4Title = "Password Manager";
+const pos4Text = "It creates strong reuseable site specific passwords from a simple input. Doesn't even need to save a hashed password";
+const pos4Git = "https://github.com/AdamJackman/OnePassword";
+import pos4Img from './content/oneWord.jpg'
+const pos5Title = "RIP Grooveshark";
+const pos5Text = "Chrome extension to track songs that would periodically get silently removed from your Grooveshark playlists.";
+const pos5Git = "https://github.com/AdamJackman/RIPgrooveshark";
+import pos5Img from './content/ripapp.png'
+const pos6Title = "TLA";
+const pos6Text = "Personal project scripting business specific logic. Made my development team much more efficient.";
+import pos6Img from './content/TLA.png'
 
 class CompletedProjects extends React.Component {
   //render
@@ -28,17 +33,17 @@ class CompletedProjects extends React.Component {
     return(
     <div className="CompletedProjects-section">
       <div className="CompletedProjects-container">
-        <Project pos={1} title={pos1Title} content={pos1Text} img={pos1Img}></Project>
-        <Project pos={2} title={pos2Title} content={pos2Text} img={pos2Img}></Project>
-        <Project pos={3} title={pos3Title} content={pos3Text} img={pos3Img}></Project>
+        <Project pos={1} title={pos1Title} content={pos1Text} img={pos1Img} git={pos1Git}/>
+        <Project pos={2} title={pos2Title} content={pos2Text} img={pos2Img} git={pos2Git}/>
+        <Project pos={3} title={pos3Title} content={pos3Text} img={pos3Img} git={pos3Git}/>
       </div>
       <div className="CompleteProjects-titleContainer">
         <h1 className="CompletedProjects-title"> Projects </h1>
       </div>
       <div className="CompletedProjects-container">
-        <Project pos={4} title={pos4Title} content={pos4Text} img={pos4Img}></Project>
-        <Project pos={5} title={pos5Title} content={pos5Text} img={pos5Img}></Project>
-        <Project pos={6} title={pos6Title} content={pos6Text} img={pos6Img}></Project>
+        <Project pos={4} title={pos4Title} content={pos4Text} img={pos4Img} git={pos4Git}/>
+        <Project pos={5} title={pos5Title} content={pos5Text} img={pos5Img} git={pos5Git}/>
+        <Project pos={6} title={pos6Title} content={pos6Text} img={pos6Img}/>
       </div>
     </div>
     )
--- src/components/project/project.js
+++ src/components/project/project.js
@@ -3,15 +3,30 @@ import React, {PropTypes} from 'react'
 import './css/project.css'
 
 class Project extends React.Component {
+  //<img src={this.props.img} className={"project-logo-" + this.props.pos} alt="woopsie" />
+  constructor() {
+    super();
+    this.state = {img: false};
+  };
+  handleImage() {
+   this.setState({img: !this.state.secret});
+  };
+  //<img src={this.props.img} className={"Project-logo-" + this.props.pos} alt="woopsie" />
   render(){
+    let GithubButton = (this.props.git) ?
+      <a href={this.props.git} rel={'external'} className="btn Project-btn-github">Source Code</a>:
+      undefined
     return(
-      <div className="Project-box">
-        <img src={this.props.img} className={"project-logo-" + this.props.pos} alt="woopsie" />
-        <div className="Project-text">
-          <div className="Project-header">{this.props.title}</div>
-          <div className="Project-body">{this.props.content}</div>
+        <div className="Project-box">
+          <div className="Project-text">
+            <div className="Project-header">{this.props.title}</div>
+            <div className="Project-body">{this.props.content}</div>
+          </div>
+          <div className="Project-btn-group">
+            <div className="btn Project-btn-image">Image</div>
+            {GithubButton}
+          </div>
         </div>
-      </div>
     )
   }
 }
@@ -19,6 +34,7 @@ Project.PropTypes = {
   pos: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
-  img: PropTypes.object.isRequired
+  img: PropTypes.object.isRequired,
+  git: PropTypes.string
 }
 export default Project;
