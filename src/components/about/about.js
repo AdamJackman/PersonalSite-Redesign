1d00b6ff3615ffbc91f8d4c6b683b0a866ba19ea
AdamJackman <adam.jackman@clara.com>
2017-07-10 21:00:47 -0700

    fix company zillow link.

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -3,7 +3,6 @@ import React from 'react';
 import me from './content/adam.jpg'
 import github from './content/otherGithub.svg'
 import linkedIn from './content/linkedInColour.svg'
-import gmail from './content/gmail.svg'
 import twitter from './content/twitterColour.svg'
 
 import './css/about.css'
@@ -13,20 +12,26 @@ class About extends React.Component {
   render(){
     return (
       <div className="About-container">
-        <img
-          className="About-img"
-          src={me}
-          alt="me"/>
+        <div className="About-img-container">
+          <img
+            className="About-img"
+            src={me}
+            alt="me"
+          />
+        </div>
         <div className="container">
           <div className="About-header">Welcome to my corner of the internet</div>
           <div className="About-body">
             This site exists to hold a portfolio of my current and on-going work.<br />
             I hope you enjoy your stay. Feel free to contact me with any questions or bugs.<br />
             <div className="About-media">
-              <a href="https://github.com/AdamJackman" rel={'external'}><div className="Media-item btn-github Media-github"> <img className="social-img" src={github} alt="GitHub" /></div></a>
+              <a href="https://github.com/AdamJackman" rel={'external'}>
+                <div className="Media-item btn-github Media-github">
+                  <img className="social-img" src={github} alt="GitHub" />
+                </div>
+              </a>
               <a href="https://www.linkedin.com/in/adamjackman" rel={'external'}><div className="Media-item btn-facebook Media-linkedIn"><img className="social-img" src={linkedIn} alt="LinkedIn"/></div></a>
-              <a href="mailto:adamjackmandevelopment@gmail.com"><div className="Media-item btn-adn Media-email"><img className="social-img" src={gmail} alt="Gmail"/></div></a>
-              <a href="https://twitter.com/adamdjackman" rel={'external'}><div className="Media-item btn-twitter Media-twitter"> <img className="social-img" src={twitter} alt="Twitter" /></div></a>
+              <a href="https://twitter.com/jackmanadam" rel={'external'}><div className="Media-item btn-twitter Media-twitter"> <img className="social-img" src={twitter} alt="Twitter" /></div></a>
             </div>
           </div>
         </div>
--- src/components/currentWork/currentWork.js
+++ src/components/currentWork/currentWork.js
@@ -13,8 +13,10 @@ class CurrentWork extends React.Component {
         <h1 className="CurrentWork"> Current Work: </h1>
         <div className="CurrentWork-container">
           <h1 className="CurrentWork-title">
-            <img className="CurrentWork-companyLogo" src={claraLogo} alt="clara"/>
-            <p className="CurrentWork-current">- Software Engineer</p>
+            <div className="CurrentWork-logo-container">
+              <img className="CurrentWork-companyLogo" src={claraLogo} alt="clara"/>
+            </div>
+            <p className="CurrentWork-current">- Full Stack Software Engineer</p>
           </h1>
           <div className="CurrentWork-tab">
             <div className="CurrentWork-imageDiv"><img className="CurrentWork-img" src={home} alt="stock home"/></div>
@@ -24,12 +26,14 @@ class CurrentWork extends React.Component {
                 We are a diverse group of experts putting our heads together trying <br />
                 to make getting that house the easiest and cheapest process possible. <br />
                 <br />
+
                 For me this involves building tech that will help build an efficient process, <br />
                 share context between the team, automate work, fact check the team <br />
                 and promote the best and most affordable options for the borrower <br />
                 <br />
-                Check out our reviews on Zillow: <a href={"https://www.zillow.com/lender-profile/marketing282/"} rel={'external'} className="CurrentWork-link">here</a><br /><br />
-                Check out our blog: <a href={"https://clara.com/blog/"} rel={'external'} className="CurrentWork-link">here</a><br />
+                <div className="work-link"> Check out the Clara website: <a href={"https://www.clara.com"} rel={'external'} className="CurrentWork-link">here</a></div>
+                <div className="work-link"> Check out our reviews on Zillow: <a href={"https://www.zillow.com/lender-profile/claralending/"} rel={'external'} className="CurrentWork-link">here</a></div>
+                <div className="work-link"> Check out our blog: <a href={"https://clara.com/blog/"} rel={'external'} className="CurrentWork-link">here</a></ div>
               </div>
             </div>
           </div>
